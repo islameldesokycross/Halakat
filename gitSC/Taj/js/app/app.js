7c5356650a2837f3a18437bd802313afae411fba
Islameldesoky <islameldesoky@gmail.com>
2014-06-15 23:25:08 +0300

    taj commit

---
+++ gitSC/Taj/js/app/app.js
@@ -0,0 +1,274 @@
+﻿// create the module and name it scotchApp
+var tajApp = angular.module('tajApp', ['ngRoute', 'ui.bootstrap', 'hammer', 'tajApp.services']);
+
+// configure our routes
+tajApp.config(function ($routeProvider) {
+    $routeProvider
+
+     //route for the home page
+    .when('/', {
+        templateUrl: 'pages/home/home.html',
+        controller: 'homeController'
+    })
+    .when('/search', {
+        templateUrl: 'pages/search/search.html',
+        controller: 'searchController'
+    })
+    .when('/branch/:branchId', {
+        templateUrl: 'pages/branch/branch.html',
+        controller: 'branchController'
+    })
+    .when('/section?myParam1&myParam2', {
+        templateUrl: 'pages/section/section.html',
+        controller: 'sectionController'
+    })
+
+    .otherwise({
+        redirectTo: '/'
+    })
+})
+
+.service('CordovaService', ['$document', '$q',
+  function ($document, $q) {
+
+      var d = $q.defer(),
+          resolved = false;
+
+      var self = this;
+      this.ready = d.promise;
+
+      document.addEventListener('deviceready', function () {
+          resolved = true;
+          d.resolve(window.cordova);
+      });
+
+      setTimeout(function () {
+          if (!resolved) {
+              if (window.cordova) d.resolve(window.cordova);
+          }
+      }, 3000);
+  }])
+
+.factory('Notification', function ($q, $window, CordovaService) {
+    return {
+        alert: function (message, alertCallback, title, buttonName) {
+            CordovaService.ready().then(function () {
+                $window.navigator.notification.alert(message, alertCallback, title, buttonName);
+            });
+        }
+    };
+})
+
+.run(function (CordovaService) {
+    FastClick.attach(document.body);
+})
+
+.controller('mainController', function ($scope, $location, CordovaService, mainServices) {
+    
+    mainServices.updatedCategoryCount(1200);
+
+    CordovaService.ready.then(function () {
+        Notification.alert('cordova ready', function () { }, 'Alert', 'OK');
+        document.addEventListener("menubutton", function () { alert('menu button clicked'); }, false);
+    });
+
+    console.log('+_');
+    $scope.menuOpened = false;
+
+
+    $scope.menuHidden = false;
+    $scope.searchHidden = false;
+
+
+    $scope.openMenu = function () {
+        console.log($scope.menuOpened);
+        $scope.menuOpened = !$scope.menuOpened;
+    }
+
+    $scope.closeMenu = function () {
+        if ($scope.menuOpened == true) $scope.menuOpened = false;
+    }
+
+    $scope.go = function (path) {
+        $location.path(path);
+    }
+})
+
+.controller('homeController', function ($scope) {
+    $scope.$parent.menuOpened = false;
+    $scope.$parent.menuHidden = false;
+    $scope.$parent.searchHidden = false;
+})
+
+.controller('searchController', function ($scope) {
+    $scope.$parent.menuOpened = false;
+    $scope.$parent.menuHidden = true;
+    $scope.$parent.searchHidden = true;
+})
+
+.controller('branchController', function ($scope) {
+    $scope.$parent.menuOpened = false;
+    $scope.$parent.menuHidden = false;
+    $scope.$parent.searchHidden = false;
+
+    /* start */
+    //for debug
+    window.scope = $scope;
+    $scope.oneAtATime = true;
+
+    $scope.groups = [
+    {
+        title: "Dynamic Group Header - 1",
+        content: "Dynamic Group Body - 1",
+        subtree: [{
+            title: "subDynamic Group Header - 1",
+            content: "subDynamic Group Body - 2",
+        }, {
+            title: "subDynamic Group Header - 2",
+            content: "Dynamic Group Body - 2",
+        },
+        {
+            title: "subDynamic Group Header - 3",
+            content: "Dynamic Group Body - 2",
+        }]
+    },
+      {
+          title: "Dynamic Group Header - 2",
+          content: "Dynamic Group Body - 2",
+      }
+    ];
+
+    $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+
+    $scope.vars = { dhTraslate: 0 };
+
+
+    $scope.left = function ($index) {
+        console.log('left', Math.abs($scope.vars.dhTraslate));
+        var tt = "caresoule" + $index;
+        var width = document.getElementById(tt).clientWidth;
+        if (Math.abs($scope.vars.dhTraslate) > 2) {
+            $scope.vars.dhTraslate += width / 5;
+        }
+    };
+
+    $scope.right = function ($index) {
+        var tt = "caresoule" + $index;
+        var width = document.getElementById(tt).clientWidth;
+        if (Math.abs($scope.vars.dhTraslate) < width) {
+            $scope.vars.dhTraslate -= width / 5;
+        }
+    };
+
+    $scope.handleGesture = function (ev, i) {
+        switch (ev.type) {
+            case 'swipeleft':
+                $scope.right(i);
+                ev.gesture.stopDetect();
+                break;
+            case 'swiperight':
+                $scope.left(i);
+                ev.gesture.stopDetect();
+                break;
+            case 'dragright':
+                console.log(ev.gesture.deltaX);
+                if (Math.abs(ev.gesture.deltaX) > 5) {
+                    $scope.left(i);
+                }
+                break;
+            case 'dragleft':
+                console.log(ev.gesture.deltaX);
+
+                if (Math.abs(ev.gesture.deltaX) > 5) {
+                    $scope.right(i);
+                }
+                break;
+        }
+
+    };
+
+
+    $scope.style = function () {
+        return {
+            "-webkit-transform": "translate3d(" + $scope.vars.dhTraslate + "px, 0, 0)",
+            "-moz-transform": "translate3d(" + $scope.vars.dhTraslate + "px, 0, 0)",
+            "-ms-transform": "translate3d(" + $scope.vars.dhTraslate + "px, 0, 0)",
+            "-o-transform": "translate3d(" + $scope.vars.dhTraslate + "px, 0, 0)",
+            "transform": "translate3d(" + $scope.vars.dhTraslate + "px, 0, 0)",
+        };
+    };
+
+    /* end */
+
+})
+
+.controller('sectionController', function ($scope) {
+    $scope.$parent.menuOpened = false;
+    $scope.$parent.menuHidden = false;
+    $scope.$parent.searchHidden = false;
+
+    $scope.activeSec = 'books';
+    $scope.setActiveSec = function (secName) { $scope.activeSec = secName }
+
+    /**/
+    $scope.vars = { dhTraslate: 0 };
+
+
+    $scope.left = function () {
+        var tt = "caresoule" ;
+        var width = document.getElementById(tt).clientWidth;
+        if (Math.abs($scope.vars.dhTraslate) > 2) {
+            $scope.vars.dhTraslate += width / 5;
+        }
+    };
+
+    $scope.right = function () {
+        var tt = "caresoule" ;
+        var width = document.getElementById(tt).clientWidth;
+        if (Math.abs($scope.vars.dhTraslate) < width) {
+            $scope.vars.dhTraslate -= width / 5;
+        }
+    };
+
+    $scope.handleGesture = function (ev) {
+        switch (ev.type) {
+            case 'swipeleft':
+                $scope.right();
+                ev.gesture.stopDetect();
+                break;
+            case 'swiperight':
+                $scope.left();
+                ev.gesture.stopDetect();
+                break;
+            case 'dragright':
+                console.log(ev.gesture.deltaX);
+                if (Math.abs(ev.gesture.deltaX) > 5) {
+                    $scope.left();
+                }
+                break;
+            case 'dragleft':
+                console.log(ev.gesture.deltaX);
+
+                if (Math.abs(ev.gesture.deltaX) > 5) {
+                    $scope.right();
+                }
+                break;
+        }
+
+    };
+
+
+    $scope.style = function () {
+        return {
+            "-webkit-transform": "translate3d(" + $scope.vars.dhTraslate + "px, 0, 0)",
+            "-moz-transform": "translate3d(" + $scope.vars.dhTraslate + "px, 0, 0)",
+            "-ms-transform": "translate3d(" + $scope.vars.dhTraslate + "px, 0, 0)",
+            "-o-transform": "translate3d(" + $scope.vars.dhTraslate + "px, 0, 0)",
+            "transform": "translate3d(" + $scope.vars.dhTraslate + "px, 0, 0)",
+        };
+    };
+
+    $scope.alert = function(){alert()}
+    /**/
+})
---
+++ gitSC/Taj/js/app/services.js
@@ -0,0 +1,83 @@
+﻿
+var tajAppAppServices = angular.module('tajApp.services', []);
+
+// service utils services 
+tajAppAppServices.factory("serviceUtils", function ($http/*, log*/) {
+
+    var utils = {};
+    
+
+    // main service URL
+    utils.serviceURL = 'http://tajalfhars.com/taj/index.php?op=smartphone&';
+
+   
+    // default ajax obj
+    utils.settings = {
+        method: 'POST',
+        url: utils.serviceURL,
+        data: null,
+        params: null,
+        headers: null,
+        timeout: 20000,
+        cache: false,
+        responseType: "",
+        success: function (data, status, headers, config) {
+            console.log(data.replace(/'/g, '"'));
+        },
+        error: function (data, status, headers, config) {
+            console.log("http error > " + status + ": " + JSON.stringify(data));
+        }
+    };
+
+    // generic method to be used across any http request
+    // input : object of ajax params
+    utils.callService = function (params) {
+        //check if any of the params is undefined
+        var ajaxStngs = angular.extend({}, utils.settings, params);
+        if (ajaxStngs.success == undefined) ajaxStngs.success = utils.settings.success;
+        if (ajaxStngs.error == undefined) ajaxStngs.error = utils.settings.error;
+        $http({
+            method: ajaxStngs.method,
+            url: utils.serviceURL + ajaxStngs.url,
+            data: (ajaxStngs.method == 'POST' && ajaxStngs.data != null ) ?
+                 JSON.stringify(ajaxStngs.data) : ajaxStngs.data,
+            params: ajaxStngs.params,
+            headers: ajaxStngs.headers,
+            timeout: ajaxStngs.timeout,
+            cache: ajaxStngs.cache,
+            responseType: ajaxStngs.responseType,
+        })
+        .success(ajaxStngs.success)
+        .error(ajaxStngs.error);
+    };
+    return utils;
+});
+
+tajAppAppServices.filter('replace2Json', function () {
+    return function (text) {
+        var json = text.replace(/'/g, '"');
+        return json;
+    }
+});
+
+// post services factory
+tajAppAppServices.factory("mainServices", function (serviceUtils) {
+
+    var updatedCategoryCount = function (lastupdate, successFn, errorFn) {
+
+        serviceUtils.callService({
+            method: 'GET',
+            url: 'query=updatedcategorycount&lastupdate= ' + lastupdate,
+            data: null,
+            success: successFn,
+            error: errorFn
+        });
+    }
+
+    
+
+    return {
+        updatedCategoryCount: updatedCategoryCount
+    };
+});
+
