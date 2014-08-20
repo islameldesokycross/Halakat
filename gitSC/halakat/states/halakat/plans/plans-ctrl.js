94aa79efacd05b576f1f840164c9cf39f4d771bd
Islameldesoky <islameldesoky@gmail.com>
2014-08-20 20:33:37 +0300

    k

--- gitSC/halakat/states/halakat/plans/plans-ctrl.js
+++ gitSC/halakat/states/halakat/plans/plans-ctrl.js
@@ -1,9 +1,30 @@
-﻿var plansCtrl = ['$scope', '$state', function ($scope, $state) {
+﻿var plansCtrl = ['$scope', '$state', '$modal', function ($scope, $state, $modal) {
 
     $scope.vars = {};
     $scope.funs = {};
     $scope.radio1Model = '0';
     $scope.$parent.vars.titleTxt = 'الخطط';
 
+
+
+    $scope.openTasme3 = function (size) {
+
+        var modalInstance = $modal.open({
+            templateUrl: 'tasme3.html',
+            controller: ['$scope', '$modalInstance', function ($scope, $modalInstance) {
+                $scope.ok = function () {
+
+                };
+
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
+            }],
+            size: size,
+            resolve: {
+
+            }
+        });
+    };
 
 }];
