b49f0d35f6dbbc496ce12b8548b2095cd85afd89
Ghazal Ahmed <eng_rahmed@yaoo.com>
2014-10-15 00:24:13 +0200

    attend report

--- gitSC/halakat/states/halakat/attendreports/attendreports-ctrl.js
+++ gitSC/halakat/states/halakat/attendreports/attendreports-ctrl.js
@@ -15,7 +15,11 @@
         negative: 0, //6
         reminder: 0 //7
     }
-       
+    $scope.fromDate = '';//TODO today's date
+    $scope.toDate = '';//TODO after month
+    $scope.studentId = '';
+    $scope.index = 1;
+
     $scope.updateCounters = function (attendanceDays) {
         for (var i in attendanceDays) {
             var day = attendanceDays[i];
@@ -47,39 +51,48 @@
         }
     }
 
-    $scope.getStudentsByRingId = function (ringId) {
-        studentServices.getAllStudentByRingId(ringId,
-            function (data) {
-                $scope.vars.ringStudents = data;
-            },
-            function (err) {
-                console.log(err);
-            })
-    };
-    $scope.getStudentsByRingId($scope.selectedRing.ID);
+    if ($scope.userType == "teacher") {
+        $scope.getStudentsByRingId = function (ringId) {
+            studentServices.getAllStudentByRingId(ringId,
+                function (data) {
+                    console.log(data);
+                    $scope.vars.ringStudents = data;
+                    $scope.studentId = data[0].Id;
+                },
+                function (err) {
+                    console.log(err);
+                })
+        };
+        $scope.getStudentsByRingId($scope.selectedRing.ID);
+    }
+    else {//TODO student
+        //$scope.studentId = '';
+    }
 
-    $scope.getAttendance = function (studentId, startDate, endDate) {
+    $scope.getAttendance = function () {
+        var studentId= $scope.studentId;
+        var startDate=$scope.fromDate;
+        var endDate = $scope.toDate;
         attendServices.GetStudentAttendance(studentId, startDate, endDate, function (data) {
             console.log(data);
             $scope.updateCounters(data)
         }, function (error) {
             console.log(error);
         })
-    }('2', '03/10/2014', '10/10/2014');
+    }
+    //('2', '03/10/2014', '10/10/2014');
 
-    $scope.opendate = function (size) {
-
+    $scope.opendate = function (index, fromDate, toDate) {
+        $scope.index = index;
         var modalInstance = $modal.open({
             template: $templateCache.get('date.html'),
-            controller: ['$scope', '$modalInstance', function ($scope, $modalInstance) {
-
-
+            controller: ['$scope', '$modalInstance', 'index', 'fromDate', 'toDate',
+                function ($scope, $modalInstance, index, fromDate, toDate) {
                 $scope.modalVars = {
                     selectedD: 0,
                     selectedM: 0,
                     selectedY: 0
                 };
-
 
                 $scope.getRange = function (n, m) {
                     return _.range(n, m);
@@ -122,25 +135,47 @@
                       //:
                       $scope.getRange(1, $scope.picker.getDaysInMonth(selY, selM));
 
-                    alert($scope.modalVars.selectedD +  $scope.modalVars.selectedM + $scope.modalVars.selectedY);
                 }
 
-
                 $scope.ok = function () {
-                    
+                    console.log($scope.modalVars)
+                    if (index==1) {
+                        fromDate = $scope.modalVars.selectedM + '/' + $scope.modalVars.selectedD + '/' + $scope.modalVars.selectedY;
+                    }
+                    else {
+                        toDate = $scope.modalVars.selectedM + '/' + $scope.modalVars.selectedD + '/' + $scope.modalVars.selectedY;
+                    }
+                    console.log(fromDate)
+                    console.log(toDate)
+                    $modalInstance.dismiss('cancel');
                 };
 
                 $scope.cancel = function () {
                     $modalInstance.dismiss('cancel');
                 };
             }],
-            size: size,
             resolve: {
-
+                index: function () {
+                    return $scope.index;
+                },
+                fromDate: function () {
+                    return $scope.fromDate;
+                },
+                toDate: function () {
+                    return $scope.toDate;
+                }
             }
         });
+        //modalInstance.result.then(function () {
+        //    if ($scope.fromDate != '' && $scope.toDate != '') {
+        //        $scope.getAttendance($scope.studentId, $scope.fromDate, $scope.toDate)
+        //    }
+        //    console.log($scope.fromDate)
+        //    console.log($scope.toDate)
+        //}, function () {
+        //    $scope.fromDate = fromDate;
+        //    $scope.toDate = toDate;
+        //})
     };
     
-    
-
 }];
