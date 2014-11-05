074b2638bfdb1e09a14c5bde51df0c0fde81fa97
Ghazal Ahmed <eng_rahmed@yaoo.com>
2014-11-05 02:04:51 +0200

    notes 3

--- gitSC/halakat/states/halakat/tsmi3/tsmi3-ctrl.js
+++ gitSC/halakat/states/halakat/tsmi3/tsmi3-ctrl.js
@@ -9,14 +9,15 @@ function ($scope, $state, studentServices, planServices, $timeout, tsmi3Assignme
     $scope.student = '';
     $scope.plan = "";
     $scope.startSura = "";
-    $scope.date = { actualDate: '', assig: {}, plan: '' };
+    $scope.date = { actualDate: '',scheduleDate:'', assig: {}, plan: '',type:'' };
 
     $scope.$parent.vars.titleTxt = 'التسميع';
-    $scope.spin = false;
+    $scope.spinning = false;
 
-    $scope.opendate = function (assig, date) {
+    $scope.opendate = function (assig,no, date) {
         $scope.date.assig = assig;
         $scope.date.plan = $scope.plan;
+        $scope.date.type = no;
 
         var modalInstance = $modal.open({
             template: $templateCache.get('date.html'),
@@ -71,14 +72,32 @@ function ($scope, $state, studentServices, planServices, $timeout, tsmi3Assignme
                       $scope.getRange(1, ($scope.picker.currentDay + 1));
                     }
                 }
+                $scope.daysBetween = function (date1, date2) {
+                    //Get 1 day in milliseconds
+                    var one_day = 1000 * 60 * 60 * 24;
 
+                    // Convert both dates to milliseconds
+                    var date1_ms = date1.getTime();
+                    var date2_ms = date2.getTime();
+
+                    // Calculate the difference in milliseconds
+                    var difference_ms = date2_ms - date1_ms;
+
+                    // Convert back to days and return
+                    return Math.round(difference_ms / one_day);
+                }
                 $scope.ok = function () {
                     console.log($scope.modalVars)
-                    date.actualDate = $scope.modalVars.selectedD + '/' + $scope.modalVars.selectedM + '/' + $scope.modalVars.selectedY;
-                    $('#' + assig.Id).text(date.actualDate);
-                    date.assig.ActualDate = date.actualDate;
-                    date.assig.done = true;
-
+                    if (date.type==2) {
+                        date.actualDate = $scope.modalVars.selectedM + '/' + $scope.modalVars.selectedD + '/' + $scope.modalVars.selectedY;
+                        date.assig.ActualDate = date.actualDate;
+                        date.assig.done = true;
+                    }
+                    else {
+                        date.scheduleDate = $scope.modalVars.selectedM + '/' + $scope.modalVars.selectedD + '/' + $scope.modalVars.selectedY;
+                        date.assig.ScheduledDate = date.scheduleDate;
+                    }
+                    date.assig.DayDifferent = $scope.daysBetween(new Date(date.assig.ActualDate), new Date(date.assig.ScheduledDate));
                     var d = '';
                     var x = $.calendars.newDate(parseInt($scope.modalVars.selectedY), parseInt($scope.modalVars.selectedM),
                         parseInt($scope.modalVars.selectedD), "Islamic", "ar");
@@ -111,26 +130,26 @@ function ($scope, $state, studentServices, planServices, $timeout, tsmi3Assignme
     };
 
     $scope.getStudentsByRingId = function (ringId) {
-        $scope.spin = true;
+        $scope.spinning = true;
         studentServices.getAllStudentByRingId(ringId,
             function (data) {
-                $scope.spin = false;
+                $scope.spinning = false;
                 $scope.vars.ringStudents = data;
                 $scope.student = $scope.vars.ringStudents[0].Id;
                 $scope.getPlans();
             },
             function (err) {
-                $scope.spin = false;
+                $scope.spinning = false;
                 console.log(err);
             })
     };
     $scope.getStudentsByRingId($scope.selectedRing.ID);
 
     $scope.getPlans = function () {
-        $scope.spin = true;
+        $scope.spinning = true;
         planServices.getAllPlansByStudentId($scope.student,
                         function (data) {
-                            $scope.spin = false;
+                            $scope.spinning = false;
                             $scope.vars.studentPlans = data;
                             $scope.plan = $scope.vars.studentPlans[0].Id;
                             $scope.getTsme3Data();
@@ -138,43 +157,57 @@ function ($scope, $state, studentServices, planServices, $timeout, tsmi3Assignme
                             console.log($scope.vars.studentPlans)
                         },
                         function (err) {
-                            $scope.spin = false;
+                            $scope.spinning = false;
                             console.log(err)
                             if (err.ErrorDes == "No available Saving Plan") {
                                 $scope.vars.studentPlans = [];
-                                $scope.spin = false;
+                                $scope.spinning = false;
                             }
                         });
     };
 
     $scope.getTsme3Data = function () {
-        $scope.spin = true;
+        $scope.spinning = true;
         tsmi3AssignmentServices.getRecitationPlanAndAssignmentsByStudentIdAndSavingPlanID($scope.plan,
             $scope.student, function myfunction(data) {
-                $scope.spin = false;
+                $scope.spinning = false;
                 $scope.vars.tsme3Records = data;
                 if ($scope.vars.tsme3Records.RecitationPlan.SwraStart) {
                     $scope.startSura = "البداية" + ' ' + QuranData.suras.sura[$scope.vars.tsme3Records.RecitationPlan.SwraStart].name;
+                    $scope.AyaStart = $scope.vars.tsme3Records.RecitationPlan.AyaStart;
                 }
                 for (var i in $scope.vars.tsme3Records.RecitationPlanAssignments) {
+                    $scope.vars.tsme3Records.RecitationPlanAssignments[i].StartSura = QuranData.suras.sura[$scope.vars.tsme3Records.RecitationPlanAssignments[i].StartSura].name
+                    var fullDate = new Date($scope.vars.tsme3Records.RecitationPlanAssignments[i].ScheduledDate);
+                    var twoDigitMonth = fullDate.getMonth() + 1;
+                    var currentSDate = twoDigitMonth + "/" + fullDate.getDate() + "/" + fullDate.getFullYear();
+                    $scope.vars.tsme3Records.RecitationPlanAssignments[i].ScheduledDate = currentSDate;
+
                     if ($scope.vars.tsme3Records.RecitationPlanAssignments[i].ActualDate != null){
                         $scope.vars.tsme3Records.RecitationPlanAssignments[i].done = true;
+                        var fullDate = new Date($scope.vars.tsme3Records.RecitationPlanAssignments[i].ActualDate);
+                        var twoDigitMonth =fullDate.getMonth() + 1;
+                        var currentADate = twoDigitMonth + "/" + fullDate.getDate() + "/" + fullDate.getFullYear();
+                        $scope.vars.tsme3Records.RecitationPlanAssignments[i].ActualDate = currentADate;
+                        $scope.vars.tsme3Records.RecitationPlanAssignments[i].DayDifferent = $scope.daysBetween(new Date(currentSDate), new Date(currentADate));
                     }
                     else {
                         $scope.vars.tsme3Records.RecitationPlanAssignments[i].done = false;
                     }
-                    $scope.vars.tsme3Records.RecitationPlanAssignments[i].StartSura = QuranData.suras.sura[$scope.vars.tsme3Records.RecitationPlanAssignments[i].StartSura].name
+                    
+
+                    
                 }
                 console.log('tsmeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee3');
                 console.log(data)
             }, function (error) {
-                $scope.spin = false;
+                $scope.spinning = false;
                 console.log(error);
             })
     }
 
     $scope.update = function (assig) {
-        if (assig.ActualDate != null && assig.ActualDate!= undefined) {
+        if (assig.ActualDate != null && assig.ActualDate != undefined) {
             var dateActual = assig.ActualDate.split('/');
 
             var d1 = '';
@@ -195,15 +228,30 @@ function ($scope, $state, studentServices, planServices, $timeout, tsmi3Assignme
         } else {
             d2 = assig.ScheduledDate;
         }
-        $scope.spin = true;
+        $scope.spinning = true;
         tsmi3AssignmentServices.updateRecitationsAssignment(assig.Id, $scope.plan, d2,
             d1, assig.DayDifferent, assig.NumberOfFaults, assig.AssignmentPages, assig.EndAya,
             assig.StartAya, function (data) {
-                $scope.spin = false;
+                $scope.spinning = false;
                 console.log(data);
             }, function (error) {
-                $scope.spin = false;
+                $scope.spinning = false;
                 console.log(error);
             })
+    };
+
+    $scope.daysBetween = function (date1, date2) {
+        //Get 1 day in milliseconds
+        var one_day = 1000 * 60 * 60 * 24;
+
+        // Convert both dates to milliseconds
+        var date1_ms = date1.getTime();
+        var date2_ms = date2.getTime();
+
+        // Calculate the difference in milliseconds
+        var difference_ms = date2_ms - date1_ms;
+
+        // Convert back to days and return
+        return Math.round(difference_ms / one_day);
     }
 }];
